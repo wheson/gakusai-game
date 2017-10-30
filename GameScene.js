b2de313480da481613439f1f3223f724dd336d90
wheson <wheson.hxs@gmail.com>
2017-10-30 12:49:06 +0900

    レベル調整しやすいように同値から以下に変更

--- GameScene.js
+++ GameScene.js
@@ -171,14 +171,14 @@ phina.define("GameScene", {
                 if (randomNum <= 100) {
                     var enemy = Enemy(dir, 0, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
                 }
-            } else if (this.level === 1) {
+            } else if (this.level <= 1) {
                 // enemy0: 60%, enemy1: 40%, enemy2: 0%, enemy3: 0%
                 if (randomNum <= 60) {
                     var enemy = Enemy(dir, 0, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
                 } else if (randomNum <= 100) {
                     var enemy = Enemy(dir, 1, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
                 }
-            } else if (this.level === 2) {
+            } else if (this.level <= 2) {
                 // enemy0: 30%, enemy1: 20%, enemy2: 50%, enemy3: 0%
                 if (randomNum <= 30) {
                     var enemy = Enemy(dir, 0, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
@@ -187,7 +187,7 @@ phina.define("GameScene", {
                 } else if (randomNum <= 100) {
                     var enemy = Enemy(dir, 2, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
                 }
-            } else if (this.level === 3) {
+            } else if (this.level <= 3) {
                 // enemy0: 30%, enemy1: 10%, enemy2: 50%, enemy3: 10%
                 if (randomNum <= 30) {
                     var enemy = Enemy(dir, 0, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
@@ -198,7 +198,7 @@ phina.define("GameScene", {
                 } else if (randomNum <= 100) {
                     var enemy = Enemy(dir, 3, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
                 }
-            } else if (this.level === 4) {
+            } else if (this.level <= 4) {
                 // enemy0: 30%, enemy1: 10%, enemy2: 50%, enemy3: 10%
                 if (randomNum <= 30) {
                     var enemy = Enemy(dir, 0, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
@@ -209,7 +209,7 @@ phina.define("GameScene", {
                 } else if (randomNum <= 100) {
                     var enemy = Enemy(dir, 3, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
                 }
-            } else if (this.level === 5) {
+            } else if (this.level <= 5) {
                 // enemy0: 30%, enemy1: 30%, enemy2: 30%, enemy3: 10%
                 if (randomNum <= 30) {
                     var enemy = Enemy(dir, 0, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
@@ -220,21 +220,21 @@ phina.define("GameScene", {
                 } else if (randomNum <= 100) {
                     var enemy = Enemy(dir, 3, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
                 }
-            } else if (this.level === 6) {
+            } else if (this.level <= 6) {
                 // enemy0: 0%, enemy1: 70%, enemy2: 0%, enemy3: 30%
                 if (randomNum <= 70) {
                     var enemy = Enemy(dir, 1, this.level, this.tomapiko.x, this.tomapiko.y).addChildTo(this.enemyGroup);
                 } else if (randomNum <= 100) {
                     var enemy = Enemy(dir, 3, this.level, this.tomapiko.x, this.tomapiko.y).addChildTo(this.enemyGroup);
                 }
-            } else if (this.level === 7) {
+            } else if (this.level <= 7) {
                 // enemy0: 0%, enemy1: 70%, enemy2: 0%, enemy3: 30%
                 if (randomNum <= 70) {
                     var enemy = Enemy(dir, 1, this.level, this.tomapiko.x, this.tomapiko.y).addChildTo(this.enemyGroup);
                 } else if (randomNum <= 100) {
                     var enemy = Enemy(dir, 3, this.level, this.tomapiko.x, this.tomapiko.y).addChildTo(this.enemyGroup);
                 }
-            } else if (this.level === 8) {
+            } else if (this.level <= 8) {
                 // enemy0: 30%, enemy1: 30%, enemy2: 30%, enemy3: 10%
                 if (randomNum <= 30) {
                     var enemy = Enemy(dir, 0, this.level, Random.randint(0, SCREEN_WIDTH), Random.randint(0, SCREEN_HEIGHT)).addChildTo(this.enemyGroup);
@@ -268,9 +268,9 @@ phina.define("GameScene", {
 			var dir = Random.randint(0,3);
             if (this.level === 0)
                 var item = Item(dir, Random.randint(1, 65)).addChildTo(this.itemGroup);
-            else if (this.level < 2)
+            else if (this.level <= 2)
                 var item = Item(dir, Random.randint(1, 85)).addChildTo(this.itemGroup);
-            else if (this.level < 4)
+            else if (this.level <= 4)
                 var item = Item(dir, Random.randint(1, 95)).addChildTo(this.itemGroup);
             else
                 var item = Item(dir, Random.randint(1, 100)).addChildTo(this.itemGroup);
