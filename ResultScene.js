b993905083777b0e421317410120ae038d40ca35
vividorange <fegi3.4@gmail.com>
2017-11-03 09:26:26 +0900

    終了ラベルを削除

--- ResultScene.js
+++ ResultScene.js
@@ -12,13 +12,6 @@ phina.define("ResultScene", {
         this.bg.width = SCREEN_WIDTH;
         this.bg.height = SCREEN_HEIGHT;
 		
-        this.gameTitle = Label("終了");
-        this.gameTitle.addChildTo(this);
-        this.gameTitle.x = 50;
-        this.gameTitle.y = 50;
-        this.gameTitle.fill = 'black';
-        this.gameTitle.fontSize = 15;
-		
 		// やられトマピコ
 		var tomapiko = Tomapiko().addChildTo(this).setPosition(SCREEN_WIDTH/2,this.gridY.span(5));
 		tomapiko.animation.gotoAndPlay("down");
